7e79d7302be22dee7929338680133330b99d210b
Arjun Mahishi <arjun.mahishi@gmail.com>
2020-10-03 01:01:37 +0530

    Updates

---
+++ precache-manifest.9b35d6f2cac9b434e3da6f47ce08e374.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "176bb1de6df159318367be8b3304094f",
+    "url": "/type-test/index.html"
+  },
+  {
+    "revision": "c4ef29739ab2897cc9e8",
+    "url": "/type-test/static/css/main.117c5173.chunk.css"
+  },
+  {
+    "revision": "d715e1f69372c8063a62",
+    "url": "/type-test/static/js/2.8285a742.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/type-test/static/js/2.8285a742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4ef29739ab2897cc9e8",
+    "url": "/type-test/static/js/main.4dafd9d2.chunk.js"
+  },
+  {
+    "revision": "1252accf563c5c6d7edd",
+    "url": "/type-test/static/js/runtime-main.45f6a901.js"
+  }
+]);
