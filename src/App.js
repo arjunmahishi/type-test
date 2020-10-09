1dad3a5a87f06c75b10b315667a4ec336ab59d28
SAUMYAK <saumyak@gl63-8rc.Home>
2020-10-10 00:43:02 +0530

    ADD: Git star| App name

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import TextDisplay from './components/TextDisplay';
 import './css/App.css';
 import { calculateSpeed } from "./controllers/speed"
 import { startTimer, stopTimer } from "./controllers/timer"
+import GitHubButton from "react-github-btn"
 
 const NumberOfWords = 5
 
@@ -30,12 +31,16 @@ class App extends React.Component {
       speed: calculateSpeed(NumberOfWords, stopTimer()),
     })
     this.timerStarted = false
-
   }
 
   render() {
     return (
-      <div className="App">
+      <>
+      <div className="gitBtn">
+        <GitHubButton href="https://github.com/arjunmahishi/type-test" data-color-scheme="no-preference: light; light: light; dark: dark;" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star arjunmahishi/type-test on GitHub">Star</GitHubButton>
+      </div>
+      <h1 className="appName">type-test</h1>
+      <div className="App">       
         <h2 className="speed">{this.state.speed} wpm</h2>
         <TextDisplay 
           typedText={this.state.typedText} 
@@ -48,6 +53,7 @@ class App extends React.Component {
           onChange={this.handleInput} 
         />
       </div>
+      </>
     );
   }
 }
