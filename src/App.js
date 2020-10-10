cd33795672e45a5d87534ec410388f2ccc97f1d1
Saumyak Pandey <saumyak.pandey@gmail.com>
2020-10-10 22:04:13 +0530

    ADD: Reset button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './css/App.css';
 import { calculateSpeed } from "./controllers/speed"
 import { startTimer, stopTimer } from "./controllers/timer"
 import GitHubButton from "react-github-btn"
+import Button from '@material-ui/core/Button';
 
 const NumberOfWords = 5
 
@@ -33,27 +34,37 @@ class App extends React.Component {
     this.timerStarted = false
   }
 
+  restart = () =>{
+    this.setState({
+      typedText: "",
+      speed: 0
+    })
+  }
+
   render() {
     return (
-      <>
+    <>
       <div className="gitBtn">
         <GitHubButton href="https://github.com/arjunmahishi/type-test" data-color-scheme="no-preference: light; light: light; dark: dark;" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star arjunmahishi/type-test on GitHub">Star</GitHubButton>
       </div>
       <h1 className="appName">type-test</h1>
-      <div className="App">       
-        <h2 className="speed">{this.state.speed} wpm</h2>
+      
+      <Button color="secondary" className="restartBtn" onClick={this.restart}>RESTART</Button>
+     
+      <div className="App">
+        <h2 className="speed">{this.state.speed} wpm</h2>             
         <TextDisplay 
-          typedText={this.state.typedText} 
+          typedText={this.state.typedText}
           resetCallback={this.resetInput}
-          numberOfWords={NumberOfWords} 
+          numberOfWords={NumberOfWords}
         />
-        <input value={this.state.typedText} 
-          id="text-input" 
-          className="text-input" 
-          onChange={this.handleInput} 
+        <input value={this.state.typedText}
+          id="text-input"
+          className="text-input"
+          onChange={this.handleInput}
         />
       </div>
-      </>
+    </>
     );
   }
 }
