c5284b942834420ad27774e23cd794987f4c0840
Arjun Mahishi <arjun.mahishi@gmail.com>
2020-10-12 00:26:24 +0530

    FIX: control the word background colors through css class names

--- src/components/TextDisplay.js
+++ src/components/TextDisplay.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import '../css/TextDisplay.css'
 
-const colors = {
-    correct: "rgba(119, 219, 119, 0.1)",
-    wrong: "rgba(255, 0, 0, 0.1)",
-    inactive: "#202020",
-}
-
 export default function TextDisplay(props) {
     return (
         <div className="words-holder">
-            {props.words.map((word, id) => {
-                return <span 
-                style={{backgroundColor: colors[word.state]}} 
-                className="word" key={id}>{word.value}</span>
-            })} 
+            {props.words.map((word, id) => 
+                <span 
+                className={"word word-" + word.state} 
+                key={id}
+                >{word.value}</span>
+            )} 
         </div>
     )
 }
